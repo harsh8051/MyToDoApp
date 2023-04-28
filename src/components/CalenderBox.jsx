4705c209704dc20215b179cce0bb0c6ed7109ce8
harsh8051 <105968770+harsh8051@users.noreply.github.com>
2023-04-28 22:45:59 +0530

    Setup css module and first css Module for Calender

--- src/components/CalenderBox.jsx
+++ src/components/CalenderBox.jsx
@@ -1,15 +1,16 @@
 import React from "react";
+import calenderBox from '../styles/calenderBox.module.css'
 
 function CalenderBox() {
   const days = Array.from({ length: 30 }, (_, i) => i + 1);
   const today = new Date().getDate();
 
   return (
-    <div className="calendar">
-      <div className="header">
+    <div className={calenderBox.calendar}>
+      <div className={calenderBox.header}>
         <h1>April 2023</h1>
       </div>
-      <div className="weekdays">
+      <div className={calenderBox.weekdays}>
         <div>Sun</div>
         <div>Mon</div>
         <div>Tue</div>
@@ -18,9 +19,9 @@ function CalenderBox() {
         <div>Fri</div>
         <div>Sat</div>
       </div>
-      <div className="days">
+      <div className={calenderBox.days}>
         {days.map((day) => (
-          <div className={day > today ? "day-active" : "day"} key={day}>
+          <div className={day > today ? calenderBox.dayActive : calenderBox.day } key={day}>
             {day}
           </div>
         ))}
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    modules: {
+      // enable CSS Modules
+      auto: true,
+      // set the generated class names
+      localsConvention: 'camelCase',
+      localIdentName: 'myapp-[name]__[local]--[hash:base64:5]'
+    }
+  }
 })
