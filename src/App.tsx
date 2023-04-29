77933d0c24710ac99d891530b487b15e867898b1
harsh8051 <105968770+harsh8051@users.noreply.github.com>
2023-04-29 15:41:19 +0530

    Local setup

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,22 @@ import './App.css'
 
 import Home from './pages/Home'
 
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 function App() {
 
   return (
-    <>
-        <Home/>
-    </>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home/>
+          <List listData={listData} />
+        </Route>
+        <Route path="/details/:title">
+          <DetailsPage listData={listData} />
+        </Route>
+      </Switch>
+    </Router>
   )
 }
 
