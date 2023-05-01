7ab1bcd9e59decb346df19d628b47773d986f297
harsh8051 <105968770+harsh8051@users.noreply.github.com>
2023-05-01 23:59:42 +0530

    Implement edit list feature

--- src/components/Input/PopupWindow.tsx
+++ src/components/Input/PopupWindow.tsx
@@ -1,17 +1,24 @@
 import {useState} from 'react'
 
+interface Task {
+  id: Number | null;
+  taskTitle: string;
+}
+
 interface Props{
-    handleSubmit:(task:string)=>void,
+    task:Task,
+    handleSubmit:(task:Task)=>void,
     handleCancel:()=>void
 };
 
-function TaskPrompt({handleSubmit,handleCancel}:Props) {
 
-    const [inputValue, setInputValue] = useState('');
+function TaskPrompt({task,handleSubmit,handleCancel}:Props) {
+
+    const [inputValue, setInputValue] = useState(task);
     
 
     function handleInputChange(event:any) {
-        setInputValue(event.target.value);
+        setInputValue({id:task.id,taskTitle:event.target.value});
     }
 
     function onSubmit(){
@@ -23,7 +30,7 @@ function TaskPrompt({handleSubmit,handleCancel}:Props) {
     <div style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%', background: 'rgba(0,0,0,0.5)', display: 'flex', justifyContent: 'center', alignItems: 'center',zIndex:'2'}}>
     <div style={{ background: '#fff', padding: '20px', borderRadius: '10px' }}>
       <h2>Enter Task Title</h2>
-      <input type="text" value={inputValue} onChange={handleInputChange} />
+      <input type="text" value={inputValue.taskTitle} onChange={handleInputChange} />
       <div style={{ display: 'flex', justifyContent: 'flex-end', marginTop: '10px' }}>
         <button onClick={onSubmit}>Submit</button>
         <button onClick={handleCancel}>Cancel</button>
--- src/pages/TaskList.tsx
+++ src/pages/TaskList.tsx
@@ -1,51 +1,78 @@
-import {useState} from 'react'
-import TaskPrompt from '../components/Task/TaskPrompt'
-import taskListPage from '../styles/taskListPage.module.scss'
+import { useState } from "react";
+import TaskPrompt from "../components/Input/PopupWindow";
+import taskListPage from "../styles/taskListPage.module.scss";
+
+interface Task {
+  id: Number | null;
+  taskTitle: string;
+}
 
 function TaskList() {
-
-  const [tasks, setTasks] = useState([
-    'Hello',
-    'How are you?',
-    'What are you up to today?',
+  const [tasks, setTasks] = useState<Task[]>([
+    { id: 1, taskTitle: "Task 1" },
+    { id: 2, taskTitle: "Task 2" },
+    { id: 3, taskTitle: "Task 3" },
   ]);
-  
+
   const [isInputVisible, setIsInputVisible] = useState(false);
+  const [taskItem, setTaskItem] = useState<Task>({ id: null, taskTitle: "" });
 
   function handleButtonClick() {
     setIsInputVisible(true);
   }
 
-  function handleSubmit(inputValue:string) {
+  function handleSubmit(inputValue: Task) {
     // Do something with the inputValue, like submit it to a server or display it on the page
-    console.log('Input value:', inputValue);
-    handleAddTask(inputValue)
+    console.log("Input value:", inputValue);
+    if (inputValue.id === null) handleAddTask(inputValue);
+    else handleTaskEdit(inputValue);
     setIsInputVisible(false);
-}
-  
-
-  function handleAddTask(task:string) {
-    //const newTask = 'This is a new message';
-    setTasks([...tasks, task]);
+    setTaskItem({ id: null, taskTitle: "" })
   }
- 
+
+  function handleAddTask(task: Task) {
+    //const newTask = 'This is a new message';
+    console.log(task);
+    const newTask = {
+      id: tasks.length+1,
+      taskTitle: task.taskTitle,
+    };
+    setTasks([...tasks, newTask]);
+  }
+
   function handleCancel() {
     // Reset the input value and hide the input box
     setIsInputVisible(false);
   }
- 
-  
+
+  function handleDoubleClick(task: Task) {
+    console.log("Double-clicked item:", task);
+    setTaskItem(task);
+    setIsInputVisible(true);
+  }
+
+  function handleTaskEdit(task: Task) {
+    console.log(task);
+    const tasksCopy = tasks.slice();
+    const index = tasksCopy.findIndex((t) => t.id === task.id);
+    tasksCopy[index] = { ...task /*updated properties here*/ };
+    setTasks(tasksCopy);
+  }
 
   return (
     <div className={taskListPage.mainbox}>
       <button onClick={handleButtonClick}>Add Task</button>
       {isInputVisible && (
-        <TaskPrompt handleSubmit={handleSubmit} handleCancel={handleCancel}/>
+        <TaskPrompt
+          task={taskItem}
+          handleSubmit={handleSubmit}
+          handleCancel={handleCancel}
+        />
       )}
       <ol className={taskListPage.gradientlist}>
         {tasks.map((task, index) => (
-          <li key={index}>
-            {task}
+          <li onDoubleClick={() => handleDoubleClick(task)} key={index}>
+            {task.taskTitle}
           </li>
         ))}
       </ol>
